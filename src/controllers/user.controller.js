331a3e91a594ddec9da37bea0780b2b198bf235c
shubham sharma <shubhamtech004@gmail.com>
2024-10-07 00:19:45 +0530

    pipeline dine

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,8 @@ import{User} from "../models/user.model.js"
 import{uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose"
+import { use } from "bcrypt/promises.js"
 
 const genratteAcessAndRefereshToken = async(userId) =>{
     try {
@@ -393,6 +395,58 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
 })
  
 
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },{
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avtar:1
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                    ,{
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+       return res.status(200)
+       .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "watch histroy fetch "
+        )
+       )
+})
+
 
 export {
     registerUser, 
@@ -404,5 +458,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImg,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
